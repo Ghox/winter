ac7b767634462a5f3ba15d03edbdc811e2303d89
hugoChav <huchav24@gmail.com>
2016-03-17 14:00:26 -0600

    added some routes in routes/users.js

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,26 @@
 /**
  * Created by rd-hc on 17/03/16.
  */
+var express = require('express');
+var router = express.Router();
+
+
+router.route('/')
+    .get(function(request, response){
+        response.json({'get':true});
+    })
+    .post(function(request, response){
+        response.json({'post':true});
+    });
+
+router.route('/:name')
+    .get(function(request, response){
+       response.send(request.params.name);
+    })
+    .post(function(request, response){
+        response.send(request.query.name);
+    });
+
+
+
+module.exports = router;
